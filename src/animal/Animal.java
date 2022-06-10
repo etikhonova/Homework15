04f21550a487bbfcb40714eb4e9452c6526027bd
etikhonova <hell.me.mi@gmail.com>
2022-06-10 16:18:12 +0300

    make Animal abstract

--- src/animal/Animal.java
+++ src/animal/Animal.java
@@ -1,34 +1,40 @@
 package animal;
 
-public class Animal {
+public abstract class Animal {
+
     protected String name;
     protected int age;
     protected int weight;
     protected String color;
+
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
     }
+
     public int getAge() {
         return age;
     }
     public void setAge(int age) {
         this.age = age;
     }
+
     public int getWeight() {
         return weight;
     }
     public void setWeight(int weight) {
         this.weight = weight;
     }
+
     public String getColor() {
         return color;
     }
     public void setColor(String color) {
         this.color = color;
     }
+
     public void say () {
         System.out.println("Я говорю");
     }
@@ -41,6 +47,7 @@ public class Animal {
     public void Drink () {
         System.out.println("Я пью");
     }
+
     public String ageName (int age){
         int lastNumber10 =  (age % 10);
         int lastNumber100 = (age % 100);
@@ -52,6 +59,7 @@ public class Animal {
         else if (lastNumber10 == 4) return " года";
         else return " лет";
     }
+    
     @Override
     public String toString() {
         return "Привет! меня зовут " +name+ ", мне "+ age + ageName(age)+ ", я вешу - "+weight+" кг"+", мой цвет - "+color;
